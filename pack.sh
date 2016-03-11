f8b0907e1f5196689506215553484782cdcdfe7a
klink-mit-panzerlip <klink.von.stalag13@gmail.com>
2016-03-11 14:52:56 +0000

    update 1.15

--- pack.sh
+++ pack.sh
@@ -2,6 +2,7 @@
 PACK=francois_was_not_so_cosmopolitaine
 DEST=~/.local/share/Paradox\ Interactive/Europa\ Universalis\ IV/mod
 
+mkdir -p "$DEST"
 rm -fv "$DEST/$PACK"*
-#cp -v "$PACK".mod "$DEST"/
+cp -v "$PACK"/"$PACK".mod "$DEST"/
 cp -rv "$PACK" "$DEST"/
